49e57b43df7ed12331c7d11971a95626136e1f22
MattADodd <121894934+MattADodd@users.noreply.github.com>
2025-03-29 20:37:45 -0700

    testing swagger

--- Backend/server.js
+++ Backend/server.js
@@ -195,7 +195,33 @@ const authenticateAdmin = async (req, res, next) => {
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// **User Registration Route**
+/**
+@swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Registers a new user with first name, email, and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request, missing fields
+ *       500:
+ *         description: Server error
+ */
 app.post("/register", async (req, res) => {
   const { firstName, email, password, isAdmin } = req.body;
   if (!firstName || !email || !password) return res.status(400).json({ error: "All fields required" });
